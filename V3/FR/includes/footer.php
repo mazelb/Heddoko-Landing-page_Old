a89e311a584c8f616ea122157fa315b6c138133c
maelbawab <mazen.elbawab@gmail.com>
2015-06-03 12:31:46 -0400

    fixed google analytics

--- V3/FR/includes/footer.php
+++ V3/FR/includes/footer.php
@@ -64,8 +64,15 @@
 </footer>
 <!-- Footer -->
 
-<!-- JavaScript 
+<!-- JavaScript -->
 <script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-52056832-1', 'heddoko.com');
+		ga('send', 'pageview');
         // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -73,7 +80,7 @@
 
         // ga('create', 'UA-52056832-1', 'heddoko.com');
         // ga('send', 'pageview');
-</script>-->
+</script>
 
 <!-- <script src="js/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
 <script src="js/rs-plugin/js/jquery.themepunch.revolution.min.js"></script> -->

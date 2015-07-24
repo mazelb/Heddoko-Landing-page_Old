d4546f0a74f60ac296575337d75afdbf2882ccfd
frnkly <frank@frnk.ca>
2015-07-24 11:22:11 -0400

    Copied Tom's testimonial to the french website

--- V3/FR/includes/about/testimonials.php
+++ V3/FR/includes/about/testimonials.php
@@ -3,7 +3,7 @@
 		<div class="row">
 
 			<div class="col-xs-2 col-sm-2 col-md-1 col-lg-1">
-				<a class="prev"><img src="../images/icons/icon-arrowleft.svg" /></a>
+				<a class="prev"><img src="../../images/icons/icon-arrowleft.svg" /></a>
 			</div>
 
 
@@ -11,7 +11,7 @@
 				<div id="testimonial-slider" class="owl-carousel" data-animated="0">
 					<div>
 						<div class="col-xs-12 col-md-4 image">
-							<img src="../images/about/ben.jpg">
+							<img src="../../images/about/ben.jpg">
 						</div>
 						<div class="col-xs-12 col-md-8 content">
 							<p>"Imagine a world where your shirt coaches your yoga, your stance, or your push-ups. Heddoko is making this a reality with a fantastic blend of both technical and marketing capabilities that they apply to everything they do. We love working with them and are proud to be part of their journey."<br/><em>- Ben O’Brien, CEO, Stretchsense</em></p>
@@ -19,7 +19,7 @@
 					</div>
 					<div>
 						<div class="col-xs-12 col-md-4 image">
-							<img src="../images/about/xavier.jpg">
+							<img src="../../images/about/xavier.jpg">
 						</div>					
 						<div class="col-xs-12 col-md-8 content">
 							<p>"Heddoko certainly has all the right ingredients to make it to the top: a great team, a great vision, and the drive and energy to make it happen! Having them at the District 3 propels a contagious positive energy.”.<br/><em>- Xavier-Henri Herv&#233;, Director and Founder, District 3 Innovation Center</em></p>
@@ -27,7 +27,7 @@
 					</div>
 					<div>
 						<div class="col-xs-12 col-md-4 image">
-							<img src="../images/about/maryse.jpg">
+							<img src="../../images/about/maryse.jpg">
 						</div>
 						<div class="col-xs-12 col-md-8 content">
 							<p>"Futurpreneur Canada is proud to have helped Heddoko launch their business and pair them with a mentor. Mazen Elbawab is a great Futurpreneur ambassador and we wish him and his team much success!"<br/><em>- Maryse Gingras, Regional Director, Futurpreneur</em></p>
@@ -35,7 +35,7 @@
 					</div>
 					<div>
 						<div class="col-xs-12 col-md-4 image">
-							<img src="../images/about/liette.jpg">
+							<img src="../../images/about/liette.jpg">
 						</div>					
 						<div class="col-xs-12 col-md-8 content">
 							<p>"En sélectionnant Heddoko parmi les meilleures entreprises en démarrage de 2014, la Fondation Montréal inc. a reconnu la solide expérience et la complémentarité de son équipe d’entrepreneurs et le fort potentiel de rayonnement que l’entreprise apporte à Montréal dans le domaine du vêtement intelligent."<br/><em>- Liette Lamonde, Executive Director, Fondation Montreal inc.</em></p>
@@ -43,7 +43,7 @@
 					</div>
 					<div>
 						<div class="col-xs-12 col-md-4 image">
-							<img src="../images/about/charlotte.jpg">
+							<img src="../../images/about/charlotte.jpg">
 						</div>
 						<div class="col-xs-12 col-md-8 content">
 							<p>"I can see a lot of potential to use the Heddoko suit as a coaching tool while I’m coaching my athletes. Combined with live coaching cues from the coach, feedback from Heddoko could potentially aid the athlete to actually see clearly where changes in their movement patterns need to be made."<br/><em>- Charlotte Greenwood, CrossFit athlete and coach</em></p>
@@ -51,17 +51,25 @@
 					</div>
 					<div>
 						<div class="col-xs-12 col-md-4 image">
-							<img src="../images/about/emmalyn.jpg">
+							<img src="../../images/about/emmalyn.jpg">
 						</div>					
 						<div class="col-xs-12 col-md-8 content">
 							<p>"Posture and alignment are the base of a solid yoga practice. Heddoko will revolutionize the way we move on the mat, making the practice safer and allowing all yoga practitioners to excel towards their goals and intentions."<br/><em>- Emmalynn Chalmers, Yoga instructor and personal trainer</em></p>
 						</div>
 					</div>
+                    <div>
+                        <div class="col-xs-12 col-md-4 image">
+                            <img src="../../images/about/emrich.jpg">
+                        </div>
+                        <div class="col-xs-12 col-md-8 content">
+                            <p>"Wearable technology is helping us understand the most important machine of all, our body! A big part of this is how we move and Heddoko's smart compression garment gives us insight into our movement like never before, providing us with the data we need to take everything to the next level"<br/><em>- Tom Emrich, Wearable tech expert and founder of "We are Wearables"</em></p>
+                        </div>
+                    </div>
 				</div>
 			</div>
 
 			<div class="col-xs-2 col-sm-2 col-md-1 col-lg-1">
-				<a class="next"><img src="../images/icons/icon-arrowright.svg" /></a>
+				<a class="next"><img src="../../images/icons/icon-arrowright.svg" /></a>
 			</div>
 
 

2bf2bef6abe409d9a99d1b3500270b8e7fe06841
frnkly <frank@frnk.ca>
2015-09-22 12:59:52 -0400

    Updated featured list

--- V3/includes/index/featured.php
+++ V3/includes/index/featured.php
@@ -1,4 +1,4 @@
-<!-- Awards -->
+<!-- Featured -->
 <section id="featured">
 	<div class="container">
 		<div class="row">
@@ -9,19 +9,19 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<div class="col-md-4 cq-info">
-					<img src="images/index/award_wearable.jpg" alt="Huffington Post">
+					<img src="images/index/featured_huffington.png" alt="Huffington Post">
 					<p>Huffington Post</p>
 				</div>
 				<div class="col-md-4 cq-info">
-					<img src="images/index/award_startupfestival.jpg" alt="Wired">
+					<img src="images/index/featured_wired.png" alt="Wired">
 					<p>Wired</p>
 				</div>
 				<div class="col-md-4 cq-info">
-					<img src="images/index/award_c2mtl.jpg" alt="Outside Magazine">
+					<img src="images/index/featured_outside.jpg" alt="Outside Magazine">
 					<p>Outside Magazine</p>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
-<!-- Awards -->
+<!-- Featured -->

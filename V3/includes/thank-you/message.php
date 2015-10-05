01733f89e5df63c315a273e853ebae647349abeb
frnkly <frank@frnk.ca>
2015-10-05 17:54:05 -0400

    Added thank you page

---
+++ V3/includes/thank-you/message.php
@@ -0,0 +1,21 @@
+
+<!-- Menu padding -->
+<div id="intro">
+	<div class="container">
+		<div class="col-md-12">
+		</div>
+	</div>
+</div>
+
+<!-- Main content -->
+<div class="main-content">
+	<div class="container">
+
+        <h1 class="page-title">Thank you/Merci ! <span></span></h1>
+
+	    <br>
+	    <br>
+	    <br>
+	    <br>
+	</div>
+</div>

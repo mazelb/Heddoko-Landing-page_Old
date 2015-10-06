f904535a047536b7bd601e0a5756d7334c407709
frnkly <frank@frnk.ca>
2015-10-06 15:20:05 -0400

    Updated styles to look like Sign Up page

--- V3/includes/thank-you/message.php
+++ V3/includes/thank-you/message.php
@@ -1,21 +1,15 @@
 
-<!-- Menu padding -->
-<div id="intro">
-	<div class="container">
-		<div class="col-md-12">
-		</div>
-	</div>
-</div>
-
-<!-- Main content -->
-<div class="main-content">
-	<div class="container">
-
-        <h1 class="page-title">Thank you/Merci ! <span></span></h1>
-
-	    <br>
-	    <br>
-	    <br>
-	    <br>
-	</div>
-</div>
+<section class="thank-you">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-5 col-mg-5 col-lg-5">
+                <h1>Thanks for signing up!</h1>
+                <h2>Welcome to the <em>Heddoko community</em></h2>
+            </div>
+            <div class="col-xs-12 col-md-5 col-mg-5 col-lg-5 col-md-offset-2 col-mg-offset-2 col-lg-offset-2">
+                <h1>Merci pour votre inscription!</h1>
+                <h2>Bienvenue dans la <em>communauté Heddoko</em></h2>
+            </div>
+        </div>
+    </div>
+</section>

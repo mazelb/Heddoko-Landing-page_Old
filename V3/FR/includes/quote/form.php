92fbcb1e93847a9709feddc5423f5be0edde9003
frnkly <frank@frnk.ca>
2015-10-06 14:52:25 -0400

    Added quotations form

---
+++ V3/FR/includes/quote/form.php
@@ -0,0 +1,20 @@
+
+<!-- Menu padding -->
+<div id="intro">
+	<div class="container">
+		<div class="col-md-12">
+		</div>
+	</div>
+</div>
+
+<!-- Form -->
+<div class="main-content">
+	<div class="container">
+
+        <h1 class="page-title">Demandez un devis <span></span></h1>
+
+	    <form>
+        
+        </form>
+	</div>
+</div>
---
+++ V3/quote/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--[if IE 8]>			<html class="ie ie8" lang="en"> <![endif]-->
+<!--[if IE 9]>			<html class="ie ie9" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!-->	<html lang="en"
+							  xmlns:og="http://ogp.me/ns#"
+     						  xmlns:fb="https://www.facebook.com/2008/fbml"> <!--<![endif]-->
+<head>
+    <?php
+
+    // Include meta tags.
+    include('../includes/meta.php');
+
+    // Define base name and filename for stylesheet.
+    $fileName = basename(__DIR__);
+    $self = array_filter(explode("/", $_SERVER['PHP_SELF']));
+    $stylesheet = $self[1] == 'FR' ? '../..' : '..';
+    $stylesheet .= '/css/'. $fileName .'.css?20151006';
+    ?>
+
+	<link rel="stylesheet" href="<?= $stylesheet ?>">
+</head>
+
+<body id="page-top" class="template-<?= $fileName; ?>">
+
+<?php include('../includes/header.php'); ?>
+
+<?php include('../includes/quote/form.php'); ?>
+
+<?php include('../includes/quote/javascript.php'); ?>
+
+<?php include('../includes/footer.php'); ?>
+
+</body>
+</html>

9f9348bd1757c183b9ae611fbe023a3d818ccbe3
Andrea Horqque <andrea.shb@hotmail.com>
2015-11-02 14:44:03 -0500

    make videos pinnable

--- V3/php/HeddokoHelperClass.php
+++ V3/php/HeddokoHelperClass.php
@@ -12,6 +12,8 @@ class Heddoko
      * @param array $options
      * @return string
      */
+
+
     public static function pin(array $options)
     {
         // Retrieve Pinterest options.
@@ -19,11 +21,8 @@ class Heddoko
         $media = isset($options['media']) ? $options['media'] : '';
         $description = isset($options['description']) ? $options['description'] : '';
 
-        // Retrieve style options
         $pos = isset($options['position']) ? $options['position'] : 'top:40px;left:40px;';
-        
-        // Temporary
-        return '';
+
 
         return  '<div style="position:absolute;z-index:100;'. $pos .'">'.
                     '<a data-pin-color="red"'.
@@ -38,4 +37,36 @@ class Heddoko
                     '</a>'.
                 '</div>';
     }
+
+     public static function pinVid(array $options)
+    {
+        // Retrieve Pinterest options.
+        $youtubelink = isset($options['youtubelink']) ? $options['youtubelink'] : '' ;
+        //$('object param[name="movie"]').val(); // grabs the video link from an embedded video
+        // OLD EMBED STYLE CODE var youtubecode = youtubelink.match(/\/v\/(.*)\?/)[1];                                                                                                             
+        $description = isset($options['description']) ? $options['description'] : '';
+
+        preg_match('@(?:^https://www\.youtube\.com/watch\?v=)([^/]+)@i',$youtubelink, $matches);
+        $youtubecode = $matches[1];
+        //youtubelink.match(/\/embed\/(.*)/)[1];
+        $pinimage = "http://img.youtube.com/vi/" . $youtubecode . "/0.jpg";
+
+        $pos = isset($options['position']) ? $options['position'] : 'top:40px;left:40px;';
+
+
+        return  '<div style="position:absolute;z-index:100;'. $pos .'">'.
+                    '<a data-pin-color="red"'.
+                        'data-pin-config="hidden"'.
+                        'data-pin-do="buttonPin"'.
+                        'href="//www.pinterest.com/pin/create/button/'.
+                            '?url='. "http://www.heddoko.com" .
+                            '&media='. $pinimage .
+                            '&description='. $description .'">'.
+
+                        '<img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_white_20.png" />'.
+                    '</a>'.
+                '</div>';
+    }
+
 }
+

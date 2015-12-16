bf00eebdfa904bf03b168a361e170db62471db0a
frnkly <frank@frnk.ca>
2015-12-16 11:51:04 -0500

    Created test page for offline signup form

---
+++ V3/protected/ces-signup/manifest.appcache.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright Heddoko(TM) 2015, all rights reserved.
+ *
+ * @author  Francis Amankrah (frank@heddoko.com)
+ */
+
+// Headers
+header('Content-type: text/cache-manifest');
+header('Content-Disposition: attachment; filename="heddoko-ces-signup.appcache"');
+header('Cache-Control: no-cache, must-revalidate');
+
+?>
+CACHE MANIFEST
+# <?php echo time(); ?>
+
+# Explicitly cached 'master entries'
+CACHE:
+index.html
+index.php
+
+NETWORK:
+*

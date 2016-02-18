11bf6ee33c0f4d56ff8d4e02bb1af2d7f5b00ed9
frnkly <frank@frnk.ca>
2016-02-18 14:55:38 -0500

    Added partners

--- V3/includes/index/partners.php
+++ V3/includes/index/partners.php
@@ -1,9 +1,36 @@
 <section id="partners">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12"><h2>A big thanks to our partners <span></span></h2></div>
+			<div class="col-xs-12">
+                <h2>
+                    A big thanks to our partners
+                    <span></span>
+                </h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-md-2 col-md-offset-3">
+                <a href="http://www.montrealinc.ca/en" target="_blank" id="index_pg_partners_INC_btn">
+                    <div class="has-icon-img" style="background-image: url(images/index/logo_mtl_inc.svg)" data-animated="0"></div>
+                </a>
+            </div>
 
             <div class="col-xs-12 col-md-2">
+                <a href="http://ubisoft.com/" target="_blank" id="index_pg_partners_UBI_btn">
+                    <div class="has-icon-img" style="background-image: url(images/index/logo_ubisoft.svg)" data-animated="0"></div>
+                </a>
+            </div>
+
+            <div class="col-xs-12 col-md-2">
+                <a href="http://gouv.qc.ca/EN/Pages/Accueil.aspx" target="_blank" id="index_pg_partners_QC_btn">
+                    <div class="has-icon-img" style="background-image: url(images/index/logo_quebec.svg)" data-animated="0"></div>
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-md-2 col-md-offset-2">
                 <a href="http://d3center.ca/" target="_blank" id="index_pg_partners_D3_btn">
                     <div class="has-icon-img" style="background-image: url(images/index/logo_district3.svg)" data-animated="0"></div>
                 </a>
@@ -12,12 +39,6 @@
             <div class="col-xs-12 col-md-2">
                 <a href="http://www.stretchsense.com/" target="_blank" id="index_pg_partners_SS_btn">
                     <div class="has-icon-img" style="background-image: url(images/index/logo_stretchsense.svg)" data-animated="0"></div>
-                </a>
-            </div>
-
-            <div class="col-xs-12 col-md-2">
-                <a href="http://ubisoft.com/" target="_blank" id="index_pg_partners_UBI_btn">
-                    <div class="has-icon-img" style="background-image: url(images/index/logo_ubisoft.svg)" data-animated="0"></div>
                 </a>
             </div>
 
@@ -32,12 +53,6 @@
                     <div class="has-icon-img" style="background-image: url(images/index/logo_nod.svg)" data-animated="0"></div>
                 </a>
             </div>
-
-            <div class="col-xs-12 col-md-2">
-                <a href="http://gouv.qc.ca/EN/Pages/Accueil.aspx" target="_blank" id="index_pg_partners_QC_btn">
-                    <div class="has-icon-img" style="background-image: url(images/index/logo_quebec.svg)" data-animated="0"></div>
-                </a>
-            </div>
-		</div>
+        </div>
 	</div>
 </section>

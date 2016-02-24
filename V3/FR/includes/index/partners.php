9fcac1b294965629dda94586ead96aad27d63236
frnkly <frank@frnk.ca>
2016-02-24 09:54:46 -0500

    Added Tech Stars to partners section

--- V3/FR/includes/index/partners.php
+++ V3/FR/includes/index/partners.php
@@ -10,7 +10,7 @@
         </div>
 
         <div class="row">
-            <div class="col-xs-12 col-md-2 col-md-offset-3">
+            <div class="col-xs-12 col-md-2 col-md-offset-2">
                 <a href="http://www.montrealinc.ca/fr" target="_blank" id="index_pg_partners_INC_btn">
                     <div class="has-icon-img" style="background-image: url(../images/index/logo_mtl_inc.svg)" data-animated="0"></div>
                 </a>
@@ -25,6 +25,12 @@
             <div class="col-xs-12 col-md-2">
                 <a href="https://www.economie.gouv.qc.ca/accueil/" target="_blank" id="index_pg_partners_QC_btn">
                     <div class="has-icon-img" style="background-image: url(../images/index/logo_quebec_2.svg)" data-animated="0"></div>
+                </a>
+            </div>
+
+            <div class="col-xs-12 col-md-2">
+                <a href="http://www.techstars.com/content/blog/announcing-the-techstars-boston-spring-2016/" target="_blank" id="index_pg_partners_TECHSTARS_btn">
+                    <div class="has-icon-img" style="background-image: url(../images/index/logo_techstars.svg)" data-animated="0"></div>
                 </a>
             </div>
         </div>
